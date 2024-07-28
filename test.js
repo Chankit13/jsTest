924724f7ae48d6566aa51127330e29399fd24e66
Chankit13 <150265721+Chankit13@users.noreply.github.com>
2024-07-28 14:17:16 +0530

    Adding new file

--- test.js
+++ test.js
@@ -1,9 +1,7 @@
-
-let numberOfNFTs = 0;.
-
+let numberOfNFTs = 0;
 
 function mintNFT(name, description, image) {
-    let newNFT = { /
+    let newNFT = {
         name: name,
         description: description,
         image: image
@@ -12,7 +10,6 @@ function mintNFT(name, description, image) {
     return newNFT;
 }
 
-.
 function listNFTs(nftsArray) {
     nftsArray.forEach(nft => { 
         console.log("Name: " + nft.name); 
@@ -21,11 +18,9 @@ function listNFTs(nftsArray) {
     });
 }
 
-
 function getTotalSupply() {
     return numberOfNFTs; 
 }
-
 
 let nft1 = mintNFT("NFT1", "Description for NFT1", "image1.jpg"); // Minting an NFT.
 let nft2 = mintNFT("NFT2", "Description for NFT2", "image2.jpg"); // Minting another NFT.
@@ -33,4 +28,5 @@ let nft3 = mintNFT("NFT3", "Description for NFT3", "image3.jpg"); // Minting ano
 
 let nftsArray = [nft1, nft2, nft3]; 
 listNFTs(nftsArray); 
-console.log("Total Supply: " + getTotalSupply()); 
+console.log("Total Supply: " + getTotalSupply());
+
